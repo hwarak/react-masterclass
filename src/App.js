66032fcfa2e056a408b483eefcaa2a94cc4a4fc9
Minhee Seo <hwarakhada@gmail.com>
2022-05-10 15:53:43 +0900

    컴포넌트 생성시 attribute 할당 가능

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import styled from "styled-components";
 const Father = styled.div`
   display:flex;
 `
-const Btn = styled.button`
-  color:white;
-  background-color: tomato;
-  border:0;
-  border-radius: 15px;
-`
+const Input = styled.input.attrs({ required: true })`
+background-color:tomato;`
 
 function App() {
   return <Father>
-    <Btn>Log in</Btn>
-    <Btn as="a">Log in</Btn>
+    <Input />
+    <Input />
+    <Input />
+    <Input />
+    <Input />
   </Father>;
 }
 
