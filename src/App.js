6a0a522a4a916d034095512ca3a5fa4e8b256864
Minhee Seo <hwarakhada@gmail.com>
2022-05-10 15:50:35 +0900

    as 사용해서 스타일은 같지만 html 태그만 변경

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import styled from "styled-components";
 const Father = styled.div`
   display:flex;
 `
-
-const Box = styled.div`
-  background-color:${(props) => props.bgColor};
-  width:100px;
-  height:100px;
-`
-
-// Box의 모든 속성을 받아오소 border-radius를 더해줄거야
-const Circle = styled(Box)`
-  border-radius:50px;
+const Btn = styled.button`
+  color:white;
+  background-color: tomato;
+  border:0;
+  border-radius: 15px;
 `
 
 function App() {
   return <Father>
-    <Box bgColor="teal" />
-    <Circle bgColor="tomato" />
+    <Btn>Log in</Btn>
+    <Btn as="a">Log in</Btn>
   </Father>;
 }
 
