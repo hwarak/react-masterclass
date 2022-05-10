99ec0229613943f2a2e83458aee9920101b608a8
Minhee Seo <hwarakhada@gmail.com>
2022-05-10 15:40:11 +0900

    background-color porps로 넘겨주자

--- src/App.js
+++ src/App.js
@@ -4,28 +4,16 @@ const Father = styled.div`
   display:flex;
 `
 
-const Box1 = styled.div`
-  background-color:tomato;
+const Box = styled.div`
+  background-color:${(props) => props.bgColor};
   width:100px;
   height:100px;
-`
-
-const Box2 = styled.div`
-  background-color:teal;
-  width:100px;
-  height:100px;
-`
-
-const Text = styled.span`
-  color: white;
 `
 
 function App() {
   return <Father>
-    <Box1>
-      <Text>Hello</Text>
-    </Box1>
-    <Box2></Box2>
+    <Box bgColor="teal" />
+    <Box bgColor="tomato" />
   </Father>;
 }
 
