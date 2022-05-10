233818fc3163dfa073f5184b00813be187810f81
Minhee Seo <hwarakhada@gmail.com>
2022-05-10 16:01:57 +0900

    애니메이션 사용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display:flex;
 `
-const Input = styled.input.attrs({ required: true })`
-background-color:tomato;`
+
+const rotationAnimation = keyframes`
+  0% {
+    transform:  rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`
+
+const Box = styled.div`
+  height:200px;
+  width:200px;
+  background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
+  `
 
 function App() {
-  return <Father>
-    <Input />
-    <Input />
-    <Input />
-    <Input />
-    <Input />
-  </Father>;
+  return <Wrapper>
+    <Box />
+  </Wrapper>;
 }
 
 export default App;
