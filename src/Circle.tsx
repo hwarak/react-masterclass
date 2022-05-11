12b5dd0213565a7b9838cdd702e512ff66e6f16f
Minhee Seo <hwarakhada@gmail.com>
2022-05-11 09:02:01 +0900

    타입스크립로 스타일컴포넌트 사용

---
+++ src/Circle.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+import { isPropertySignature } from "typescript";
+
+interface ContainerProps {
+    bgColor:string
+}
+
+const Container = styled.div<ContainerProps>`
+    width:200px;
+    height:200px;
+    background-color: ${(props) => props.bgColor};
+    border-radius: 100px;
+`
+
+interface CircleProps {
+    // interface 안에다가 타입스크립트에게 오브젝트 모양을 설명해줄거야
+    // 타입스크립트에게 bgColor는 string이 되어야 한다고 설명해주자
+    bgColor:string
+}
+
+// CircleProps의 타입이 뭔지 Compinent에게 말해줘야함
+// 이 bgColor의 타입은 CircleProps의 object라고 말해주고있는거야
+function Circle({bgColor}:CircleProps) {
+    return <Container bgColor={bgColor}></Container>  
+}
+
+export default Circle;
