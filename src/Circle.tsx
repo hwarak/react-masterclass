7032f8599be00fa7af0173b63061f189ba8531cd
Minhee Seo <hwarakhada@gmail.com>
2022-05-11 09:19:03 +0900

    props를 optional 하는법과 default값 설정하는 법

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components"
 import { isPropertySignature } from "typescript";
 
 interface ContainerProps {
-    bgColor:string
+    bgColor: string
+    borderColor: string
 }
 
 const Container = styled.div<ContainerProps>`
@@ -10,18 +11,20 @@ const Container = styled.div<ContainerProps>`
     height:200px;
     background-color: ${(props) => props.bgColor};
     border-radius: 100px;
+    border: 5px solid ${(props) => props.borderColor}
 `
 
 interface CircleProps {
     // interface 안에다가 타입스크립트에게 오브젝트 모양을 설명해줄거야
     // 타입스크립트에게 bgColor는 string이 되어야 한다고 설명해주자
-    bgColor:string
+    bgColor: string
+    borderColor?: string
 }
 
 // CircleProps의 타입이 뭔지 Compinent에게 말해줘야함
 // 이 bgColor의 타입은 CircleProps의 object라고 말해주고있는거야
-function Circle({bgColor}:CircleProps) {
-    return <Container bgColor={bgColor}></Container>  
+function Circle({bgColor,borderColor}:CircleProps) {
+    return <Container bgColor={bgColor} borderColor={borderColor ?? "blue"}></Container>  
 }
 
 export default Circle;
