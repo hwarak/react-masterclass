a791e00bcf948d89c93fbe9a1d2e6fd70baa0f01
Minhee Seo <hwarakhada@gmail.com>
2022-05-11 10:37:36 +0900

    event에 타입 설정해주기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import styled, {keyframes} from "styled-components";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`hello ${value}`)
+  };
   return <div>
-    <Circle bgColor="teal" borderColor="black" />
-    <Circle bgColor="tomato" text="hello"/>
+    <form onSubmit={onSubmit}>
+      <input
+        value={value}
+        onChange={onChange}
+        type="text"
+        placeholder="username" />
+      <button>Log in</button>
+    </form>
   </div>;
 }
 
